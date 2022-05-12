27d2ac46315294d024bf7ed71126e2149fe3b9a6
Mwai <machariamwai@gmail.com>
2022-05-12 22:30:20 +0300

    Code along

---
+++ src/components/DisplayData.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DisplayData = (props) => {
+  return (
+    <div>
+      <h1>{props.firstName}</h1>
+      <h1>{props.LastName}</h1>
+
+    </div>
+  )
+}
+
+export default DisplayData
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
+const Form = (props) => {
   return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form>
+        <input type="text" onChange={props.handleFirstNameChange} value={props.firstName} />
+        <input type="text" onChange={props.handleLastNameChange} value={props.lastName} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
-}
+};
 
 export default Form;
